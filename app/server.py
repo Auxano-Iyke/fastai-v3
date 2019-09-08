3babdfb0803349267b2daa88a3a7504eb71407ff
Onwukwe Ikechukwu <onwukwe.ikechukwu@outlook.com>
2019-09-08 17:07:03 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,15 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = https://drive.google.com/uc?export=download&id=1-Uzwk97Lr0cEtHwJvsd6LvJ-X2ixzXBV
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['black_jeans',
+ 'blue_dress',
+ 'blue_jeans',
+ 'blue_shirt',
+ 'red_dress',
+ 'red_shirt']
 path = Path(__file__).parent
 
 app = Starlette()
