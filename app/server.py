94704ba3f62caa0d6e2610d4d2b2b655306c00a0
Onwukwe Ikechukwu <onwukwe.ikechukwu@outlook.com>
2019-09-08 17:54:35 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = https://drive.google.com/uc?export=download&id=1-Uzwk97Lr0cEtHwJvsd6LvJ-X2ixzXBV
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-Uzwk97Lr0cEtHwJvsd6LvJ-X2ixzXBV'
 export_file_name = 'export.pkl'
 
 classes = ['black_jeans',
